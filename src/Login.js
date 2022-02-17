ab581a05b90c567df725e9cf9eea064b78867e47
Clemens Frei <80027182+artemis1205@users.noreply.github.com>
2022-02-17 07:46:49 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import GoogleSignIn from "./GoogleSignIn";
 const Login = (props) => {
+const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+    
+    useEffect(() => {
+    const handler = e => {
+      e.preventDefault();
+      console.log("we are being triggered :D");
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
 
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+    
+     const onClick = evt => {
+    evt.preventDefault();
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+  if (!supportsPWA) {
+    return null;
+  }
+    
     const {email, setEmail, password, setPassword, handleLogin, handleSignup, handleLogout, hasAccount, setHasAccount, emailError, passwordError} = props;
     return (
 
@@ -26,6 +51,15 @@ const Login = (props) => {
                 
                 <p>Have an account? <span style={{color:"#5f91e8"}} onClick={() => setHasAccount(!hasAccount)}>Sign in</span></p>
                 <button className="Sign" onClick={handleSignup}>Sign up</button>
+    <button
+      className="link-button"
+      id="setup_button"
+      aria-label="Install app"
+      title="Install app"
+      onClick={onClick}
+    >
+      Install
+    </button>
                 </>
             )}
        
